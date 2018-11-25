b416926f9ddb50f0472f860f22367a62892a8d63
johnvithor <jv.venceslau.c@gmail.com>
2018-11-24 22:41:04 -0300

    Atualização nos testes

---
+++ DSATUR.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import networkx as nx
+import numpy as np
+import sys
+
+
+def ler_grafo(arquivo):
+    arq = open(arquivo, 'r')
+    texto = arq.readlines()
+    G = nx.Graph()
+    for linha in texto:
+        if(linha[0] == 'c' or linha[0] == 'p'):
+            continue
+        linha = linha.replace("\n", "")
+        partes = linha.split(" ")
+        G.add_edge(partes[1], partes[2])
+
+    arq.close()
+
+    return G
+
+
+g = ler_grafo(sys.argv[1])
+
+result = nx.greedy_color(g, strategy='DSATUR', interchange=False)
+
+cores = list(result.values())
+#print("Arquivo: " + str(sys.argv[1]) + "\n")
+print("Resultado do DSATUR: " + str(len(np.unique(cores))))
--- scrip_testes.sh
+++ scrip_testes.sh
@@ -1,4 +1,5 @@
 for f in Test_cases/*.col
 do
 	./bin/graph $f >> resultados.txt
+	python DSATUR.py $f >> resultados.txt
 done
